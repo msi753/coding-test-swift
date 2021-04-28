67c62615f9c66fe48d564fb7439d0b68c332cb97
임명심 <msi753@naver.com>
2021-04-29 01:08:55 +0900

    Initial Commit

---
+++ CodingTest/InputOutput/cat.swift
@@ -0,0 +1,17 @@
+//
+//  cat.swift
+//  CodingTest
+//
+//  Created by 명심 on 2021/04/29.
+//
+
+import Foundation
+
+let cat = #"""
+print("\\    /\\")
+print(")  ( ')")
+print("(  /  )")
+print("\\(__)|")
+"""#
+
+print(cat)
